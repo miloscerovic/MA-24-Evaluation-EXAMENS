9dbd7c0696005dde060a4a79acdfe4a664c359b5
CEROVIC Milos <Milos.CEROVIC@cpnv.ch>
2019-03-28 11:53:50 +0100

    test github

--- Evaluation_Examens/Form1.cs
+++ Evaluation_Examens/Form1.cs
@@ -16,5 +16,32 @@ namespace Evaluation_Examens
         {
             InitializeComponent();
         }
+        private void InitializeTimer()
+        {
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+
+            button1.Text = "Stop";
+            button1.Click += new EventHandler(button1_Click);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                button1.Text = "Start";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
     }
 }
